ee06a2ca36e55389c00f71db0657662d67a64dc5
Juan De Dios Baudazio <jbaudazio@git.com>
2019-08-07 02:29:48 -0500

    Agregar archivos de proyecto.

---
+++ Preguntas/Preguntas.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Examen
+{
+    partial class Preguntas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbAlternativa1 = new System.Windows.Forms.RadioButton();
+            this.lblPregunta = new System.Windows.Forms.Label();
+            this.rbAlternativa2 = new System.Windows.Forms.RadioButton();
+            this.rbAlternativa4 = new System.Windows.Forms.RadioButton();
+            this.rbAlternativa3 = new System.Windows.Forms.RadioButton();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(308, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Examen";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // rbAlternativa1
+            // 
+            this.rbAlternativa1.AutoSize = true;
+            this.rbAlternativa1.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbAlternativa1.ForeColor = System.Drawing.Color.White;
+            this.rbAlternativa1.Location = new System.Drawing.Point(59, 131);
+            this.rbAlternativa1.Margin = new System.Windows.Forms.Padding(4);
+            this.rbAlternativa1.Name = "rbAlternativa1";
+            this.rbAlternativa1.Size = new System.Drawing.Size(14, 13);
+            this.rbAlternativa1.TabIndex = 2;
+            this.rbAlternativa1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.rbAlternativa1.UseVisualStyleBackColor = true;
+            // 
+            // lblPregunta
+            // 
+            this.lblPregunta.AutoSize = true;
+            this.lblPregunta.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPregunta.ForeColor = System.Drawing.Color.White;
+            this.lblPregunta.Location = new System.Drawing.Point(55, 85);
+            this.lblPregunta.Name = "lblPregunta";
+            this.lblPregunta.Size = new System.Drawing.Size(0, 23);
+            this.lblPregunta.TabIndex = 3;
+            // 
+            // rbAlternativa2
+            // 
+            this.rbAlternativa2.AutoSize = true;
+            this.rbAlternativa2.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbAlternativa2.ForeColor = System.Drawing.Color.White;
+            this.rbAlternativa2.Location = new System.Drawing.Point(59, 164);
+            this.rbAlternativa2.Margin = new System.Windows.Forms.Padding(4);
+            this.rbAlternativa2.Name = "rbAlternativa2";
+            this.rbAlternativa2.Size = new System.Drawing.Size(14, 13);
+            this.rbAlternativa2.TabIndex = 4;
+            this.rbAlternativa2.UseVisualStyleBackColor = true;
+            // 
+            // rbAlternativa4
+            // 
+            this.rbAlternativa4.AutoSize = true;
+            this.rbAlternativa4.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbAlternativa4.ForeColor = System.Drawing.Color.White;
+            this.rbAlternativa4.Location = new System.Drawing.Point(59, 239);
+            this.rbAlternativa4.Margin = new System.Windows.Forms.Padding(4);
+            this.rbAlternativa4.Name = "rbAlternativa4";
+            this.rbAlternativa4.Size = new System.Drawing.Size(14, 13);
+            this.rbAlternativa4.TabIndex = 5;
+            this.rbAlternativa4.UseVisualStyleBackColor = true;
+            // 
+            // rbAlternativa3
+            // 
+            this.rbAlternativa3.AutoSize = true;
+            this.rbAlternativa3.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbAlternativa3.ForeColor = System.Drawing.Color.White;
+            this.rbAlternativa3.Location = new System.Drawing.Point(59, 201);
+            this.rbAlternativa3.Margin = new System.Windows.Forms.Padding(4);
+            this.rbAlternativa3.Name = "rbAlternativa3";
+            this.rbAlternativa3.Size = new System.Drawing.Size(14, 13);
+            this.rbAlternativa3.TabIndex = 6;
+            this.rbAlternativa3.UseVisualStyleBackColor = true;
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.BackColor = System.Drawing.Color.Black;
+            this.btnSiguiente.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSiguiente.ForeColor = System.Drawing.Color.White;
+            this.btnSiguiente.Location = new System.Drawing.Point(59, 275);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(94, 38);
+            this.btnSiguiente.TabIndex = 7;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.UseVisualStyleBackColor = false;
+            this.btnSiguiente.Click += new System.EventHandler(this.BtnSiguiente_Click);
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.Black;
+            this.btnRegresar.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.ForeColor = System.Drawing.Color.White;
+            this.btnRegresar.Location = new System.Drawing.Point(169, 275);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(94, 38);
+            this.btnRegresar.TabIndex = 8;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            this.btnRegresar.Click += new System.EventHandler(this.BtnRegresar_Click);
+            // 
+            // Preguntas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1161, 348);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.rbAlternativa3);
+            this.Controls.Add(this.rbAlternativa4);
+            this.Controls.Add(this.rbAlternativa2);
+            this.Controls.Add(this.lblPregunta);
+            this.Controls.Add(this.rbAlternativa1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Preguntas";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Preguntas_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbAlternativa1;
+        private System.Windows.Forms.Label lblPregunta;
+        private System.Windows.Forms.RadioButton rbAlternativa2;
+        private System.Windows.Forms.RadioButton rbAlternativa4;
+        private System.Windows.Forms.RadioButton rbAlternativa3;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Button btnRegresar;
+    }
+}
+
---
+++ Preguntas/Preguntas.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen
+{
+    public partial class Preguntas : Form
+    {
+        int resultado = 0;
+        int seleccion = 0;
+        int regresar = 0;
+        public Preguntas()
+        {
+            InitializeComponent();
+        }
+        private void Preguntas_Load(object sender, EventArgs e)
+        {
+           Limpiar();
+           pregunta1();
+        }
+        private void Limpiar()
+        {
+            this.rbAlternativa1.Checked = false;
+            this.rbAlternativa2.Checked = false;
+            this.rbAlternativa3.Checked = false;
+            this.rbAlternativa4.Checked = false;
+        }
+        private void pregunta1()
+        {
+            this.lblPregunta.Text = "1. Cual de las siguientes alternativas NO es parte de la Calidad : ";
+            this.rbAlternativa1.Text = "Aptitud de uso";
+            this.rbAlternativa2.Text = "Valor por el patrocinador(Respta)";
+            this.rbAlternativa3.Text = "Satisfacción del cliente";
+            this.rbAlternativa4.Text = "Conformidad con los requisitos";
+            seleccion++;
+            regresar++;
+        }
+        private void pregunta2()
+        {
+            this.lblPregunta.Text = "2. ¿Cuál de los siguientes NO es un ejemplo de Costo" +
+                "de la Calidad(COQ)?";
+            this.rbAlternativa1.Text = "Constractar inspectores adicionales para buscar defectos";
+            this.rbAlternativa2.Text = "Hacer que los trabajadores utilicen tiempo extra para revisar requisitos" + "\r" +
+                " de los interesados";
+            this.rbAlternativa3.Text = "Pagar programadores adicionales para ayudar a cumplir con la fecha" + "\r" +
+                "de entrega (Respta)";
+            this.rbAlternativa4.Text = "Enviar a un equipo a reparar un producto" +
+                "defectuoso que fue entregado al cliente";
+            seleccion++;
+            regresar++;
+        }
+        private void pregunta3()
+        {
+            this.lblPregunta.Text = "3. ¿Cuál de los siguientes enunciados NO es parte del " +
+                "Plan de Gestión de la Calidad?";
+            this.rbAlternativa1.Text = "Métricas para medir la calidad de su proyecto";
+            this.rbAlternativa2.Text = "Una descripción de que entregables no tienen que ser inspeccionados(Respta) ";
+            this.rbAlternativa3.Text = "Estrategias para encargarse de defectos y otros problemas de calidad" ;
+            this.rbAlternativa4.Text = "Guía de cómo el equipo de Proyecto implementará la política de calidad de la compañía" ;
+            seleccion++;
+            regresar++;
+        }
+        private void pregunta4()
+        {
+            this.lblPregunta.Text = "4. ¿Cuál de las siguientes herramientas y técnicas es utilizada para mostrar las categorías " +
+                "de defectos más comunes?";
+            this.rbAlternativa1.Text = "Diagramas de Pareto (Respta)";
+            this.rbAlternativa2.Text = "Diagramas de Control ";
+            this.rbAlternativa3.Text = "Diagramas de Comportamiento";
+            this.rbAlternativa4.Text = "Diagramas de Flujo";
+            seleccion++;
+        }
+        private void pregunta5()
+        {
+            this.lblPregunta.Text = "5. ¿Cuándo se realiza una inspección?";
+            this.rbAlternativa1.Text = "En cualquier momento que se produzca a un entregable(Respta)";
+            this.rbAlternativa2.Text = "Al Final del proyecto";
+            this.rbAlternativa3.Text = "Sólo antes de entregar el producto final";
+            this.rbAlternativa4.Text = "Al inicio del proyecto";
+            seleccion++;
+        }
+        private void pregunta6()
+        {
+            this.lblPregunta.Text = "6. ¿Qué herramienta de Control de Calidad es utilizada para analizar" +
+                                    "procesos viéndolos gráficamente?";
+            this.rbAlternativa1.Text = "Controlar la Calidad(Respta)";
+            this.rbAlternativa2.Text = "Gestionar la Calidad";
+            this.rbAlternativa3.Text = "Aseguramiento de Calidad";
+            this.rbAlternativa4.Text = "Planificar la Gestión de la Calidad";
+            seleccion++;
+        }
+        private void pregunta7()
+        {
+            this.lblPregunta.Text = "7. ¿Cuál de los siguientes está asociado a la regla 80/20?" ;
+            this.rbAlternativa1.Text = "Diagrama de Control";
+            this.rbAlternativa2.Text = "Diagrama de Dispersión";
+            this.rbAlternativa3.Text = "Diagrama de Pareto (Respta)";
+            this.rbAlternativa4.Text = "Histograma";
+            seleccion++;
+        }
+        private void pregunta8()
+        {
+            this.lblPregunta.Text = "8. ¿Cuál de las siguientes alternativas describe MEJOR la revisión de repación de defectos?";
+            this.rbAlternativa1.Text = "Revisar el defecto reparado con el interesado para asegurarse que es aceptable";
+            this.rbAlternativa2.Text = "Revisar el defecto reparado para asegurarse que está entre los limites del control";
+            this.rbAlternativa3.Text = "Revisar el defecto reparado para asegurarse que fue arreglado apropiadamente(Resta)";
+            this.rbAlternativa4.Text = "Revisar el defecto reparado con el equipo para asegurarse que documenten las lecciones aprendidas";
+            seleccion++;
+        }
+        private void pregunta9()
+        {
+            this.lblPregunta.Text = "9. ¿Una repación de defectos validada es una salidad de qué proceso?";
+            this.rbAlternativa1.Text = "Cotrol integrado de cambios";
+            this.rbAlternativa2.Text = "Controlar la calidad (Respta)";
+            this.rbAlternativa3.Text = "Gestionar la Calidad";
+            this.rbAlternativa4.Text = "Planificar la Gestión de la Calidad";
+            seleccion++;
+        }
+        private void pregunta10()
+        {
+            this.lblPregunta.Text = "10. Un Director de Proyecto está utilizando un histograma para analisar defectos" +
+                "encontrados por un equipo durante" +"\r" +" las actividades de inspección. ¿Qué proceso está desempeñando?";
+            this.rbAlternativa1.Text = "Realizar el Aseguramiento de la Calidad";
+            this.rbAlternativa2.Text = "Controlar la Calidad(Respta)";
+            this.rbAlternativa3.Text = "Planificar la Gestión de la Calidad";
+            this.rbAlternativa4.Text = "Verificar el Alcance";
+            seleccion++;
+        }
+
+        private void BtnSiguiente_Click(object sender, EventArgs e)
+        {
+            switch (seleccion)
+            {
+                case 1:
+                    if (this.rbAlternativa2.Checked == true)
+                    {
+                        resultado++;
+                    }
+                    pregunta2();
+                    Limpiar();
+                    break;
+
+                case 2:
+                    if (this.rbAlternativa3.Checked == true)
+                    {
+                        resultado++;
+                    }
+                    pregunta3();
+                    Limpiar();
+                    break;
+
+                case 3:
+                    if (this.rbAlternativa2.Checked == true)
+                    {
+                        resultado++;
+                    }
+                    pregunta4();
+                    Limpiar();
+                    break;
+
+                case 4:
+                    if (this.rbAlternativa1.Checked == true)
+                    {
+                        resultado++;
+                    }
+                    pregunta5();
+                    Limpiar();
+                    break;
+
+                case 5:
+                    if (this.rbAlternativa1.Checked == true)
+                    {
+                        resultado++;
+                    }
+                    pregunta6();
+                    Limpiar();
+                    break;
+
+                case 6:
+                    if (this.rbAlternativa1.Checked == true)
+                    {
+                        resultado++;
+                    }
+                    pregunta7();
+                    Limpiar();
+                    break;
+
+                case 7:
+                    if (this.rbAlternativa3.Checked == true)
+                    {
+                        resultado++;
+                    }
+                    pregunta8();
+                    Limpiar();
+                    break;
+
+                case 8:
+                    if (this.rbAlternativa3.Checked == true)
+                    {
+                        resultado++;
+                    }
+                    pregunta9();
+                    Limpiar();
+                    break;
+                case 9:
+                    if (this.rbAlternativa2.Checked == true)
+                    {
+                        resultado++;
+                    }
+                    pregunta10();
+                    Limpiar();
+                    break;
+                case 10:
+                    if (this.rbAlternativa2.Checked == true)
+                    {
+                        resultado++;
+                    }
+                    if (resultado == 10)
+                    {
+                        MessageBox.Show(resultado.ToString(), "FELICITACIONES", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Logro:  " + resultado.ToString() + "  Puntos", "DEBES ATENDER MAS", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Application.Exit();
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void BtnRegresar_Click(object sender, EventArgs e)
+        {
+            switch (regresar)
+            {
+                case 1:
+                    if (regresar == 1) {
+                        pregunta1();
+                    }
+                    regresar = 0;
+                    break;
+                case 2:
+                    if (regresar == 2)
+                    {
+                        pregunta2();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+}
+}
